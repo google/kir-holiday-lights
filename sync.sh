c8e82de9dde4ea315f038244ebe487208fd492b4
Eric Dalquist <eric.dalquist@gmail.com>
2014-12-12 10:39:00 -0800

    Sync cleanup

--- sync.sh
+++ sync.sh
@@ -1,1 +1,5 @@
 cp ./Sequences/* ../Sequences/
+
+# Fix file path for Eric's animation image
+sed -e 's/Z:\\git\\kir-holiday-lights/C:\\lights/g' Sequences/DoYouWantToBuildASnowMan.lms > Sequences/DoYouWantToBuildASnowMan.lms.tmp && mv Sequences/DoYouWantToBuildASnowMan.lms.tmp Sequences/DoYouWantToBuildASnowMan.lms
+
