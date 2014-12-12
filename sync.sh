c42abd8f35bf33f416d788b70544b1f14edcf063
Eric Dalquist <eric.dalquist@gmail.com>
2014-12-12 10:44:52 -0800

    Fix sync for windows

--- sync.sh
+++ sync.sh
@@ -1,5 +1,6 @@
 cp ./Sequences/* ../Sequences/
 
 # Fix file path for Eric's animation image
-sed -e 's/Z:\\git\\kir-holiday-lights/C:\\lights/g' Sequences/DoYouWantToBuildASnowMan.lms > Sequences/DoYouWantToBuildASnowMan.lms.tmp && mv Sequences/DoYouWantToBuildASnowMan.lms.tmp Sequences/DoYouWantToBuildASnowMan.lms
+sed -e 's/Z:\\git\\kir-holiday-lights/C:\\lights/g' ../Sequences/DoYouWantToBuildASnowMan.lms > ../Sequences/DoYouWantToBuildASnowMan.lms.tmp && \
+  mv ../Sequences/DoYouWantToBuildASnowMan.lms.tmp ../Sequences/DoYouWantToBuildASnowMan.lms
 
